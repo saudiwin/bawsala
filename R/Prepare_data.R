f81437863010be7788b367727b0ad8fe1ca3f589
saudiwin <bobkubinec@gmail.com>
2016-11-17 10:59:33 -0500

    fixed a bug with using NAs as a vote option

--- R/Prepare_data.R
+++ R/Prepare_data.R
@@ -132,7 +132,8 @@ clean_data <- function(keep_legis=1,use_subset=FALSE,subset_party=c("Bloc Al Hor
       x <- x %>%  mutate_at(vars(starts_with("Bill")),funs(factor(.,levels = c("contre",'abstenu',NA,'pour')) %>%
                                                              as.integer))
       if(use_nas==TRUE) {
-        x <- x %>%  mutate_at(vars(starts_with("Bill")),funs(recode(.,`3`=4,`NA`=3)))
+        x %>% mutate_at(vars(starts_with("Bill")),
+                        funs(recode(., `3` = 4L,.missing=3L)))
       }
       return(x)
     })
